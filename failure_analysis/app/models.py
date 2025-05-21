f87e82f8367c7949dcdc263f9df1d95842cafa92
IT21168390 <it21168390@my.sliit.lk>
2025-05-22 00:02:41 +0530

    (unused failure_analysis code removal)

--- failure_analysis/app/models.py
+++
@@ -1,8 +0,0 @@
-from flask_pymongo import PyMongo
-
-mongo = PyMongo()
-
-class Instructions:
-    def __init__(self, failure, details):
-        self.failure = failure
-        self.detalis = details
